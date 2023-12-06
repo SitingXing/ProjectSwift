f4995e50066e771c933cdbbae21cb7f1c3da6bd8
unknown <sitingx@umich.edu>
2023-12-05 22:28:40 -0500

    Untrack Secrets.js

--- Secrets.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDsLA1mL1zOaF9ZJa4_tdgl7ebXpF5d93w",
-    authDomain: "si-669-final-projectswift.firebaseapp.com",
-    projectId: "si-669-final-projectswift",
-    storageBucket: "si-669-final-projectswift.appspot.com",
-    messagingSenderId: "445279346211",
-    appId: "1:445279346211:web:4f9206dcad52007d3ddb96"
-  };
